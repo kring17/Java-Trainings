18c0204fb1e3a0c9932c3afad4eb9c9933c2683a
Karen R. Soh <krsoh@headoffice.corp.bpi.com.ph>
2025-12-16 15:21:07 +0800

    M2 Activity1

---
+++ Book.java
@@ -0,0 +1,11 @@
+package com.bpi.M2Activity1;
+
+public class Book {
+	
+	String title;
+	
+	void displayTitle() { //Method
+		System.out.print("The title of the book " + title);
+	}
+
+}
---
+++ SeatWork1.java
@@ -0,0 +1,15 @@
+package com.bpi.M2Activity1;
+
+public class SeatWork1 {
+
+	public static void main(String[] args) {
+		System.out.print("");
+		Book mybook = new Book(); //Create object
+		mybook.title = "Cinderellas";
+		mybook.displayTitle();		
+		
+		System.out.println();
+
+	}
+
+}
