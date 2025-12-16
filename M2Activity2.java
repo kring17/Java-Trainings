7270538d296514016a690929150ad118d379c920
Karen R. Soh <krsoh@headoffice.corp.bpi.com.ph>
2025-12-16 15:28:00 +0800

    M2 Activity2

---
+++ M2Activity2.java
@@ -0,0 +1,11 @@
+package com.bpi.M2Activity2;
+
+public class M2Activity2 {
+	public static void main(String[] args) {
+	
+		WhiteBoard wb = new WhiteBoard();
+		int total = wb.addNumbers(5, 3);
+		System.out.println("The total is: " + String.valueOf(total));
+		
+	}
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,10 @@
+package com.bpi.M2Activity2;
+
+public class WhiteBoard {
+
+	int addNumbers(int addend1, int addend2) {
+		int total = addend1 +addend2;
+		return total;
+		
+	}
+}
